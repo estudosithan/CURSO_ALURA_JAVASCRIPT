0b149e6a40e5fcdf511896c153e4cee1be6079fd
Íthan P. Amaral <ithanamaral@gmail.com>
2024-09-14 14:05:41 -0300

    Orientada a objetos

---
+++ P.O.O/classe.js
@@ -0,0 +1,53 @@
+class cliente{
+
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+
+const cliente1 = new cliente();
+const cliente2 = new cliente();
+const cliente3 = new cliente();
+
+cliente1.nome = "Íthan";
+cliente1.cpf = 10781629640;
+cliente1.agencia = 1000;
+cliente1.saldo = 2500;
+
+cliente2.nome = "Laristar";
+cliente2.cpf = 20052008;
+cliente2.agencia = 1001;
+cliente2.saldo = 5500
+
+cliente3.nome = "Manuela";
+cliente3.cpf = 20052008;
+cliente3.agencia = 1001;
+cliente3.saldo = 20000;
+
+
+
+
+
+
+const cliente4 = new cliente();
+const cliente5 = new cliente();
+const cliente6 = new cliente();
+const cliente7 = new cliente();
+const cliente8 = new cliente();
+
+console.log(cliente1);
+console.log(cliente2);
+console.log(cliente3);
+
+
+
+
+
+
+
+
+
+
+
