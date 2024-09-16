08812e0a85809d2bcc64f2b52bfb13a13dea0f3d
Íthan P. Amaral <ithanamaral@gmail.com>
2024-09-16 18:57:43 -0300

    classes moduladas

--- P.O.O/Classes.js/classe.js
+++ P.O.O/Classes.js/classe.js
@@ -1,53 +1,25 @@
-class cliente{
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"; 
 
-    nome; //Propriedade nome
-    cpf; //Propriedade cpf
-}
 
-class ContaCorrente{
-    
-    agencia; //Propriedade agência
-    saldo;  //Propriedade saldo
 
-    sacar(valor){ //Função ou método para realizar alguma operação na conta corrente, modificando a propriedade (saldo)
-        if(this.saldo >= valor){ //" this" utilizado para fazer menção a conta corrente específica que está sendo utilizada no momento
-            this.saldo -= valor;
-            return valor; //Para voltar para mim o valor que está sendo sacado para que eu possa usar no restante do programa
-        }
-    }
-    
-    depositar(valor){
-        if(valor > 0){
-            this.saldo += valor;
-            return valor; //Para voltar para mim o valor que está sendo depositado para que eu possa usar no restante do programa
-        }
-    }
+const cliente4 = new Cliente();
+const cliente5 = new Cliente();
+const cliente6 = new Cliente();
+const cliente7 = new Cliente();
+const cliente8 = new Cliente();
 
-    transferir(valor){
-        if(this.saldo > 0 && this.saldo >= valor){
-            this.saldo -= valor
-            return valor; //Para voltar para mim o valor que está sendo transferido para que eu possa usar no restante do programa
-        }
-    }
-}
-
-const cliente4 = new cliente();
-const cliente5 = new cliente();
-const cliente6 = new cliente();
-const cliente7 = new cliente();
-const cliente8 = new cliente();
-
-const cliente1 = new cliente();
+const cliente1 = new Cliente();
 cliente1.nome = "Íthan";
 cliente1.cpf = 10781629640;
 console.log(cliente1);
 
-const cliente2 = new cliente();
+const cliente2 = new Cliente();
 cliente2.nome = "Laristar";
 cliente2.cpf = 20052008;
 console.log(cliente2);
 
-const cliente3 = new cliente();
+const cliente3 = new Cliente();
 cliente3.nome = "Manuela";
 cliente3.cpf = 20052008;
 console.log(cliente3);
@@ -57,20 +29,31 @@ console.log(cliente3);
 const contaCorrenteÍthan = new ContaCorrente();
 contaCorrenteÍthan.agencia = 1002;
 contaCorrenteÍthan.saldo = 2000;
+contaCorrenteÍthan.cliente = cliente2;
 //contaCorrenteÍthan.sacar(200);
 console.log(contaCorrenteÍthan);
 
 contaCorrenteÍthan.depositar(400);
-const valorDeposito = contaCorrenteÍthan.depositar(400);
+const valorDeposito = contaCorrenteÍthan.depositar(400); //Aqui, ele considera que é necessário depositar mais 400 no saldo também
 console.log(valorDeposito);
 contaCorrenteÍthan.transferir(2800);
 console.log(contaCorrenteÍthan);
 
+//Para utilizar uma "classe dentro da outra", o nome técnico correto é instanciação
+console.log("\n\n");
+console.log(contaCorrenteÍthan);
 
+const contaCorrenteManu = new ContaCorrente();
+contaCorrenteManu.agencia = 1003;
+contaCorrenteManu.saldo = 2000;
+contaCorrenteManu.cliente = cliente3;
+//Para utilizar uma "classe dentro da outra", o nome técnico correto é instanciação
 
-
-
-
+console.log("\n\n");
+contaCorrenteÍthan.depositar(500);
+contaCorrenteÍthan.transferir(200, contaCorrenteManu);
+console.log(contaCorrenteÍthan);
+console.log(contaCorrenteManu)
 
 
 
